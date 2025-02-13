ac973ca708c153f54eb90ca2393e48a9973205cc
abdullaO7 <abdallahalhasan09@gmail.com>
2025-02-13 09:57:44 +0300

    feat /complate the sigin and tegister desgin

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -14,23 +14,32 @@ import { handleRegister } from '@/services/auth/auth.service';
 import { RegisterFormValues } from '@/zod-schemas/auth/register-form-schema';
 import Link from 'next/link';
 import { useActionState } from 'react';
+import { useLocale } from 'next-intl';
+import LocaleSwitcher from '@/components/local/LocalSwitcher';
 
 const RegisterPage = () => {
+  const locale = useLocale();
+  const isRTL = locale === 'ar';
+  
   const [state, action] = useActionState<
     ApiResponse<RegisterFormValues>,
     FormData
   >(handleRegister, defaultActionState);
 
   return (
-    <form action={action} className="w-full">
+    <form action={action} className={`w-[50%] justify-center items-center ${isRTL ? 'rtl' : 'ltr'}`} dir={isRTL ? 'rtl' : 'ltr'}>
+      
       <Box className="flex-col w-full gap-4 items-start">
+      <div className='xs:hidden lg:block md:block justify-center mx-auto'>
+                                <LocaleSwitcher/>
+                </div>
         {/* Status Message */}
         <FormStateMessage state={state} />
 
         {state.success && state.data && (
           <Text variant="link" className="text-alert">
             <Link href={allRoutes.auth.children.verifyEmail.path}>
-              resend email verification
+              {isRTL ? 'إعادة إرسال تأكيد البريد الإلكتروني' : 'Resend email verification'}
             </Link>
           </Text>
         )}
@@ -38,50 +47,48 @@ const RegisterPage = () => {
         {/* Email Input */}
         <DetailedInput
           name="email"
-          placeholder="Enter Your Email"
+          placeholder={isRTL ? 'أدخل بريدك الإلكتروني' : 'Enter Your Email'}
           type="email"
           error={state.errors?.email}
-          caption="Email"
+          caption={isRTL ? 'البريد الإلكتروني' : 'Email'}
         />
 
-        {/* Password Input */}
+        {/* Name Input */}
         <DetailedInput
           name="name"
-          placeholder="Enter Your Name"
+          placeholder={isRTL ? 'أدخل اسمك' : 'Enter Your Name'}
           type="text"
           error={state.errors?.name}
-          caption="Name"
+          caption={isRTL ? 'الاسم' : 'Name'}
         />
 
         {/* Password Input */}
         <DetailedInput
           name="password"
-          placeholder="Enter Your Password"
+          placeholder={isRTL ? 'أدخل كلمة المرور' : 'Enter Your Password'}
           type="password"
           error={state.errors?.password}
-          caption="Password"
+          caption={isRTL ? 'كلمة المرور' : 'Password'}
         />
 
-        {/* Password Input */}
+        {/* Password Confirmation Input */}
         <DetailedInput
           name="passwordConfirmation"
-          placeholder="Enter Your Password Again"
+          placeholder={isRTL ? 'أدخل كلمة المرور مرة أخرى' : 'Enter Your Password Again'}
           type="password"
           error={state.errors?.passwordConfirmation}
-          caption="Password Confirmation"
+          caption={isRTL ? 'تأكيد كلمة المرور' : 'Password Confirmation'}
         />
 
         {/* Submit Button */}
-        <SubmitButton title="Register" submittingTitle="Loading..." />
+        <SubmitButton title={isRTL ? 'تسجيل' : 'Register'} submittingTitle={isRTL ? 'جار التحميل...' : 'Loading...'} />
       </Box>
+
       <Box variant={'column'} className="items-start m-2">
         <Text>
-          Alreay have an account?{' '}
-          <Link
-            href={allRoutes.auth.children.signIn.path}
-            className="text-primary-light"
-          >
-            Sign in Now
+          {isRTL ? 'لديك حساب بالفعل؟' : 'Already have an account?'}{' '}
+          <Link href={allRoutes.auth.children.signIn.path} className="text-primary-light">
+            {isRTL ? 'تسجيل الدخول الآن' : 'Sign in Now'}
           </Link>
         </Text>
       </Box>
